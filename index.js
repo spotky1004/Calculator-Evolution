fda79f5cec6965fc0e7e4723388407b5de805594
Spotky1004 <spotky1004@gmail.com>
2020-08-18 04:47:08 +0900

    0.3

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const $ = _ => document.querySelector(_);
 const D = num => new Decimal(num);
 
-number = D(0);
-base = D(2);
-digits = D(1);
-tNow = new Date().getTime();
-programActive = [0, 0, 0];
+game = {
+  number: D(0),
+  base: D(2),
+  digits: D(1),
+  mDigits: D(6),
+  tLast: new Date().getTime(),
+  programActive: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+};
 
 function dNum(infNum) {
   return Number(infNum.valueOf());
 }
 
 function renderAll() {
-  $("#basedNumber").innerHTML = dNum(number.floor()).toString(dNum(base)).padStart(dNum(digits), 0);
+  $("#basedNumber").innerHTML = dNum(game.number.floor()).toString(dNum(game.base)).padStart(dNum(game.digits), 0).toUpperCase();
+  $("#memoryDigit").innerHTML = ("").padStart(dNum(game.mDigits)-dNum(game.digits), 0);
+  $("#numberBase").innerHTML = game.base;
   renderProgram();
 };
 function renderProgram() {
   for (var i = 0; i < 2; i++) {
-    $(".program:nth-of-type(" + (i+1) + ")").className = ((programActive[i]) ? "program active" : "program");
+    $(".program:nth-of-type(" + (i+1) + ")").className = ((game.programActive[i]) ? "program active" : "program");
   }
 }
 
@@ -25,32 +30,39 @@ function calcAll() {
   calcProgram();
 }
 function calcProgram() {
-  if (programActive[0]) {
-    number = number.add(tGain).min(base.pow(digits).sub(1));
+  if (game.programActive[0]) {
+    game.number = game.number.add(tGain).min(game.base.pow(game.digits).sub(1));
   }
-  if (programActive[1]) {
-    if (number.gte(base.pow(digits).sub(1))) {
-      number = number.sub(base.pow(digits).sub(1));
-      digits = digits.add(1);
+  if (game.programActive[1]) {
+    if (game.number.gte(game.base.pow(game.digits).sub(1)) && game.digits.lt(game.mDigits)) {
+      game.number = game.number.sub(game.base.pow(game.digits).sub(1));
+      game.digits = game.digits.add(1);
+    }
+  }
+  if (game.programActive[2]) {
+    if (game.digits.gte(game.mDigits)) {
+      game.number = D(0);
+      game.digits = D(1);
+      game.base = game.base.add(1);
     }
   }
 }
 
 function activeProgram(num) {
-  for (var i = 0; i < programActive.length; i++) {
+  for (var i = 0; i < game.programActive.length; i++) {
     if (i != num) {
-      programActive[i] = 0;
+      game.programActive[i] = 0;
     }
   }
-  programActive[num] = !programActive[num];
+  game.programActive[num] = !game.programActive[num];
   renderProgram();
 }
 
 document.addEventListener("DOMContentLoaded", function(){
   setInterval( function () {
-    tGain =  (new Date().getTime()-tNow)/1000;
+    tGain =  (new Date().getTime()-game.tLast)/100;
     calcAll();
     renderAll();
-    tNow = new Date().getTime();
+    game.tLast = new Date().getTime();
   }, 33);
 });
