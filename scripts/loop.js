560cf79edd3a52196162853838e57b4122dfe395
Spotky1004 <spotky1004@gmail.com>
2021-01-29 05:30:21 +0900

    add Simple offline progress notification (0.20.12)

--- scripts/loop.js
+++ scripts/loop.js
@@ -1,6 +1,9 @@
 var tickDone = 0;
 setInterval( function () {
   tGain = (new Date().getTime()-game.tLast)/1000*game.gameSpeed;
+  if (tGain >= 60) {
+    commandAppend(`${timeNotation(tGain)} of progress done!`, 0, 1);
+  }
   tSpeed = 0.033;
   game.tLast = new Date().getTime();
   calcAll();
