c804037ac7f0a5501534cad3ef282b06a8fd5c64
Spotky1004 <spotky1004@gmail.com>
2021-02-14 09:38:37 +0900

    fix C6 (1.0.17)

--- scripts/progress/reboot.js
+++ scripts/progress/reboot.js
@@ -31,8 +31,8 @@ function renderResearch() {
   $('.research:nth-of-type(4)').style.display = ((game.researchLevel[0]>=1) ? "inline-block" : "none");
   $('.research:nth-of-type(5)').style.display = ((game.researchLevel[0]>=1) ? "inline-block" : "none");
   $('.research:nth-of-type(6)').style.display = ((game.researchLevel[3]>=1) ? "inline-block" : "none");
-  $('.research:nth-of-type(7)').style.display = ((game.researchLevel[5]>=1) ? "inline-block" : "none");
-  $('.research:nth-of-type(8)').style.display = ((game.researchLevel[6]>=1) ? "inline-block" : "none");
+  $('.research:nth-of-type(7)').style.display = ((game.researchLevel[5]>=1||(game.challengeEntered==5&&game.researchLevel[3]>=1)) ? "inline-block" : "none");
+  $('.research:nth-of-type(8)').style.display = ((game.researchLevel[6]>=1||(game.challengeEntered==5&&game.researchLevel[3]>=1)) ? "inline-block" : "none");
 }
 function renderOverclockInfo() {
   document.getElementById('overclockInfo').style.display = ((game.researchLevel[1]>=1)?"block":"none");
